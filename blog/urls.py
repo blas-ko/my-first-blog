8bbc9ff51a854cce528ffe2d7f203dfdb8ce3ebe
Blas Kolic <blas.kolic@ponderalab.com>
2017-05-25 13:20:18 -0500

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    #?P<var> will tranfer everything put in there into the variable <var>
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
