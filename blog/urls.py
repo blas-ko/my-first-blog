19bb6486e46b97c5dee40bc9bf52e06102aeb4aa
Blas Kolic <blas.kolic@ponderalab.com>
2017-05-25 15:46:18 -0500

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     #?P<var> will tranfer everything put in there into the variable <var>
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
